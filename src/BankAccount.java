493be01cbcb3cda5db1d9fe5a0947a20ed980951
danieltanyi Github <tanyi_daniel@yahoo.com>
2022-07-19 15:41:32 +0200

    applying encapsulation on challenge solution

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,5 @@
 public class BankAccount {
-    String owner;
-    double balance;
+
     /**
      *  Add the following functionality
      *
@@ -16,4 +15,36 @@ public class BankAccount {
      *  Note: The Bank Account attributes should not be directly accessible
      *
      */
+
+
+   private   String owner;
+   private double balance;
+
+    public BankAccount(String owner, int startingbalance) {
+        this.owner = owner;
+        this.balance = Math.max(startingbalance,0) ;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double deposit (double amt){
+        if (amt >0){
+            this.balance =this.balance + amt;
+            return amt;
+        }
+        return 0;
+    }
+    public double withdraw(double amt){
+        if (amt<= this.balance){
+            this.balance =this.balance - amt;
+            return amt;
+        }
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("My Self",10000);
+
+        bankAccount.withdraw(500);
+        bankAccount.deposit(8000);
+        bankAccount.withdraw(4000);
+
+        System.out.println(bankAccount.getOwner());
+        System.out.println(bankAccount.getBalance());
 
     }
 }
