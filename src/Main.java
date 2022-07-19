e0928b4e620353c4542794f14d7714daecb3eb1a
danieltanyi Github <tanyi_daniel@yahoo.com>
2022-07-19 11:36:47 +0200

    Challenge question

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import java.awt.*;
 
+/**
+ * Create an employee class.
+ * Attributes : name, age, salary, location
+ * Behavior : raiseSalary
+ * Get creative!
+ *
+ * as part of the challenge you have to determine which member is
+ * Static or non-static
+ *
+ * Create a Main class for building Employee instances
+ * Build two Employee instances
+ * Give one Employee a raise
+ * Display the salaries of both Employees
+ *
+ */
 public class Main {
     public static void main(String[] args) {
-        Color myTrunkColor = Tree.TRUNK_COLOR;
-        Color myDefaultWhite = Color.WHITE;    // check Color.java class
-        Color myDefaultBlue = Color.BLUE;
-
-        Color brighterBlue = myDefaultBlue.brighter();
 
     }
 }
