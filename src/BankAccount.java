a0a35c4fd1fe724a5b6504403c62fe117dfb2403
danieltanyi Github <tanyi_daniel@yahoo.com>
2022-07-19 15:20:47 +0200

    applying encapsulation on challenge

---
+++ src/BankAccount.java
@@ -0,0 +1,19 @@
+public class BankAccount {
+    String owner;
+    double balance;
+    /**
+     *  Add the following functionality
+     *
+     *  Withdraw method
+     *  Deposit method
+     *  Getter methods
+     *  Constructor
+     *
+     *  Create a Main class that makes a Bank Account instance
+     *  Withdraw a certain amount using the withdrawal method
+     *  Print out the resulting balance
+     *
+     *  Note: The Bank Account attributes should not be directly accessible
+     *
+     */
+}
